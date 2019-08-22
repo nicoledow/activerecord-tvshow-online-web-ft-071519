68dd97453e367426881c0316c4d89ea8c407335a
Nicole Dow <nikki.dow71@gmail.com>
2019-08-22 01:10:39 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.all.order(rating: :desc).limit(1)[0]
+  end
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.all.order(rating: :asc).limit(1)[0]
+  end
+  
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+  
+  def self.popular_shows
+    self.all.where("rating > ?", 5)
+  end
+  
+  def self.shows_by_alphabetical_order
+    self.all.order(name: :asc)
+  end
+  
+  
+end
