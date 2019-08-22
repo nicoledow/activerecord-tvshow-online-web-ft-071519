bfa3f34a2e7e2d0ea2a4391924004cf2f206f96b
Nicole Dow <nikki.dow71@gmail.com>
2019-08-22 00:56:07 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,10 @@ class CreateShows < ActiveRecord::Migration
   
   def change
     create_table :shows do |t|
-      
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
   
